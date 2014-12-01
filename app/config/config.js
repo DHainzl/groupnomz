e800d2a5d23eb72970b842520ea90fabe3ccf44e
DHainzl <david@dhainzl.at>
2014-12-01 10:21:22 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	port: process.env.GNZ_PORT || '8080',
+	port: process.env.port || '8080',
 	sql: {
 		host: process.env.GNZ_HOST || '127.0.0.1',
 		user: process.env.GNZ_USER || 'root',
